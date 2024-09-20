b1193eba3e0fea1d8099c850a3b47ee9a46d628d
a23manuelcr <manuelcendonr@gmail.com>
2024-09-20 12:43:10 +0200

    feat(Ejercicio4): Finish part 1 of exercice

--- src/main/java/edu/badpals/Ejercicio3.java
+++ src/main/java/edu/badpals/Ejercicio3.java
@@ -3,7 +3,6 @@ package edu.badpals;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Comparator;
 import java.util.List;
---
+++ src/main/java/edu/badpals/Ejercicio4.java
@@ -0,0 +1,63 @@
+package edu.badpals;
+
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        System.out.println("Introduce el nombre del fichero");
+        Scanner sc = new Scanner(System.in);
+        String FileName = sc.nextLine();
+
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(FileName));
+            String correctAnswers = lines.get(0);
+            Map<String, Double> answers = new HashMap<>();
+
+            StringBuilder sb = null;
+            for (int i = 2; i < lines.size(); i += 2) {
+                String[] line = lines.get(i).split(" ");
+                String student = line[0];
+                String studentAnswers = line[1];
+                double score = 0;
+                for (int j = 0; j < studentAnswers.length(); j++) {
+                    if (studentAnswers.charAt(j) == correctAnswers.charAt(j)) {
+                        score++;
+                    } else if (studentAnswers.charAt(j) == ' ') {
+                        score += 0;
+
+                    } else {
+                        score -= 0.5;
+                    }
+                    answers.put(student, score);
+
+
+                }
+            }
+            sb = new StringBuilder();
+            sb.append("Resultados: ");
+            sb.append('\n');
+            for (Map.Entry<String, Double> entry : answers.entrySet()) {
+                sb.append(entry.getKey())
+                        .append(" ")
+                        .append(entry.getValue())
+                        .append(" puntos")
+                        .append('\n');
+            }
+
+            System.out.println(sb.toString());
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+}
